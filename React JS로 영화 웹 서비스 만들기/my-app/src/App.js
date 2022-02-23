50002e926b82f449a863ee908490ad017e7bceb1
이우철 <dncjf1969@naver.com>
2022-02-23 15:24:35 +0900

    useEffect

--- React JS로 영화 웹 서비스 만들기/my-app/src/App.js
+++ React JS로 영화 웹 서비스 만들기/my-app/src/App.js
@@ -1,13 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+// 어떻게 특정 코드들이 첫번째 componene render에서만 실행되게 하는지 배우고 싶다.
 
+import { useState } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('call an api');
+
   return (
     <div>
-      <h1 classname={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
-} 
+}
 
 export default App;
